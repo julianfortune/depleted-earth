772fb99188c29f202d176d670036f3c129df197f
Julian Fortune <fortunej@oregonstate.edu>
2020-11-18 23:38:23 -0800

    Cleanup

--- source/build.js
+++ source/build.js
@@ -3,7 +3,6 @@
 // Created November 18, 2020
 //
 
-const { time } = require('console');
 var fs = require('fs');
 var Handlebars = require("handlebars");
 
@@ -23,7 +22,7 @@ function createSpacer() {
 
 function createYearArray(start, stop, step) {
     roundedStart = Math.floor(start/step) * step
-    console.log(roundedStart)
+
     years = []
     for (year = roundedStart + step; year <= stop; year += step) {
         years.push(year)
@@ -31,34 +30,43 @@ function createYearArray(start, stop, step) {
     return years
 }
 
+let dataFileName = "source/data.json"
+
+console.log(`Building from '${dataFileName}' ...`)
+
 events = []
 
+// Initialize a list of year marker events in the right range
 for (year of createYearArray(2021, 2200, 5)) {
     events.push(createYear(year))
 }
 
-let dataFileName = "source/data.json"
+// Load events from the data file
 let dataFile = fs.readFileSync(dataFileName)
 let json = JSON.parse(dataFile)
 
-events = events.concat(json["contents"])
+// Put the user-created events at the end of the list so they
+// are sorted after the year markers
+events = events.concat(json["events"])
 
+// Sort the events by the year
 events = events.sort((a, b) => {
     return Number(a["year"]) - Number(b["year"])
 })
 
+// Add boolean properties to the events based on the `type`
+// property to make templating easier
 events = events.map((item) => {
     item["isResource"] = item["type"] && item["type"].toLowerCase() === "resource"
     item["isParagraph"] = item["type"] && item.type.toLowerCase() === "paragraph"
     item["isYear"] = item["type"] && item.type.toLowerCase() === "year"
-    item["isSeparator"] = item["type"] && item.type.toLowerCase() === "separator"
-    // Add other types here ...
 
     return item
 })
 
 timeline = []
 
+// Insert connecting lines and spacers as needed
 events.forEach((item, index, array) => {
     if (index > 0) {
         let priorItem = array[index - 1]
@@ -80,19 +88,20 @@ events.forEach((item, index, array) => {
     }
 })
 
+// Put the timeline back into the JSON data
 json["timeline"] = timeline
 
-console.log(`Building from '${dataFileName}' ...`)
+// Do the ✨ Handlebars magic ✨
+let templateFile = fs.readFileSync('source/template.hbs', "utf-8")
+let template = Handlebars.compile(templateFile)
+let html = template(json)
 
-var templateFile = fs.readFileSync('source/template.hbs', "utf-8")
-var template = Handlebars.compile(templateFile)
-
-var html = template(json)
-
-fs.writeFile("index.html", html, err => {
+// Output
+let htmlFile = "index.html"
+fs.writeFile(htmlFile, html, err => {
     if (err) {
         console.log(err)
     } else {
-        console.log("File written successfully")
+        console.log(`HTML written to '${htmlFile}'`)
     }
 })
